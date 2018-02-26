256a284ff190b331bcbdee39c263a13a180837c6
Joel Leach <joel.b.leach@gmail.com>
2018-02-25 20:19:37 -0500

    fixes for live stream

--- sermons_and_worship.php
+++ sermons_and_worship.php
@@ -9,41 +9,9 @@
 	<h1>Live Stream</h1>
 	<?php
 		require_once 'vendor/autoload.php';
-		
-		$API_KEY = 'AIzaSyBJTOixAJ1-Wn5F7oDd3tcx08eFPIW15Cg';
-		$ChannelID = 'UC9C4hm7oda7bYNqmyxwMtEg';
-
-		//https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC9C4hm7oda7bYNqmyxwMtEg&type=video&eventType=completed&key=AIzaSyBJTOixAJ1-Wn5F7oDd3tcx08eFPIW15Cg
-		$channelInfo = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='.$ChannelID.'&type=video&eventType=live&key='.$API_KEY;
-		$liveStreamInfo = 'https://www.youtube.com/embed/live_stream?channel='.$ChannelID;
-		
-		$extractInfo = file_get_contents($channelInfo);
-		$extractInfo = str_replace('},]',"}]",$extractInfo);
-		$showInfo = json_decode($extractInfo, true);
-
-		if($showInfo['pageInfo']['totalResults'] > 0)
-		{
-	?>
-		<div class="container">
-			<p>We are Live!</p>
-			
-			<div class="embed-responsive embed-responsive-16by9 ytp-cued-thumbnail-overlay">
-				<iframe class="embed-responsive-item" src="<?php echo $liveStreamInfo; ?>"></iframe>
-			</div>
-		</div>
-	<?php
-		} else {
-	?>
-		<div class="container">
-			
-	<?php
-			echo "We aren't currently live :(.  Our morning services are streamed live starting at 10:40 AM.  Check back then!";
-		}
-	?>
-		</div>
-	<?php
-		// $API_KEY = 'AIzaSyBJTOixAJ1-Wn5F7oDd3tcx08eFPIW15Cg';
-		// $ChannelID = 'UC9C4hm7oda7bYNqmyxwMtEg';
+	
+		$API_KEY = 'AIzaSyD1EcCt1yL5hA0CGjDpfe5x29IQnl5R3pE';
+		$ChannelID = 'UC7z_cEyyFvQ0VMAKDm7y9_w';
 		
 		$client = new Google_Client();
 		$client->setDeveloperKey($API_KEY);
@@ -56,8 +24,71 @@
 			// Call the search.list method to retrieve results matching the specified
 			// query term.
 			$searchResponse = $youtube->search->listSearch('id,snippet', array(
+			  'eventType' =>  'live',
+			  'type' => 'video',
+			  'channelId' => $ChannelID,
+			));
+		} catch (Google_Service_Exception $e) {
+			$htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+			htmlspecialchars($e->getMessage()));
+		} catch (Google_Exception $e) {
+			$htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+			htmlspecialchars($e->getMessage()));
+		}
+		
+		/* Joel's Credentials */
+		// $API_KEY = 'AIzaSyBJTOixAJ1-Wn5F7oDd3tcx08eFPIW15Cg';
+		// $ChannelID = 'UC9C4hm7oda7bYNqmyxwMtEg';
+
+		$channelInfo = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='.$ChannelID.'&type=video&eventType=live&key='.$API_KEY;
+		
+		
+		
+		
+		if($searchResponse['pageInfo']['totalResults'] > 0)
+		{
+			//https://www.youtube.com/embed/lNCKY5HSZKM?autoplay=0&origin=http://calvarychristianchurch.com
+			foreach ($searchResponse['items'] as $searchResult) {
+				$liveStreamInfo = 'https://www.youtube.com/embed/'.$searchResult['id']['videoId'].'?autoplay=0&origin=http://calvarychristianchurch.com';
+			}
+			echo $liveStreamInfo
+	?>
+		<div class="container">
+			<p>We are Live!</p>
+			
+			<!--<div class="embed-responsive embed-responsive-16by9 ytp-cued-thumbnail-overlay">
+				<iframe class="embed-responsive-item" src="<?php echo $liveStreamInfo; ?>"></iframe>
+			</div>-->
+			
+			<div class="col-sm-3 col-md-6 col-lg-12 media-col">
+				<div class="embed-responsive embed-responsive-16by9 ytp-cued-thumbnail-overlay">
+					<iframe class="embed-responsive-item" allowFullScreen='allowFullScreen' src="<?php echo $liveStreamInfo; ?>" frameborder="0"></iframe>
+				</div>
+			</div>
+		</div>
+	<?php
+		} else {
+	?>
+		<div class="container">
+			
+	<?php
+			echo "We aren't currently live :(.  Our morning services are streamed live starting at 10:40 AM.  Check back then!";
+		}
+	?>
+		</div>
+		<h1>Past Services</h1>
+	<?php
+		// $API_KEY = 'AIzaSyBJTOixAJ1-Wn5F7oDd3tcx08eFPIW15Cg';
+		// $ChannelID = 'UC9C4hm7oda7bYNqmyxwMtEg';
+		
+		
+		try {
+
+			// Call the search.list method to retrieve results matching the specified
+			// query term.
+			$searchResponse = $youtube->search->listSearch('id,snippet', array(
 			  'eventType' =>  'completed',
-			  'maxResults' => '25',
+			  'maxResults' => '10',
 			  'type' => 'video',
 			  'channelId' => $ChannelID,
 			));
@@ -78,7 +109,6 @@
 		
     
 	?>
-	<h1>Past Services</h1>
 	
 	<div class="container" style="overflow: hidden;">
 		<div>
@@ -99,8 +129,8 @@
 			<br/>
 			
 			<?php
-				//echo $searchResult['snippet']['description'];
-				echo 'this is livestream content description text.  i hope this works and contains enough text. Rachael is awesome and deserves ice cream.';
+				echo $searchResult['snippet']['description'];
+				//echo 'this is livestream content description text.  i hope this works and contains enough text. Rachael is awesome and deserves ice cream.';
 				
 				$videoURL = 'https://www.youtube.com/embed/'.$searchResult['id']['videoId'].'?autoplay=0&origin=http://calvarychristianchurch.com';
 			?>
@@ -116,42 +146,6 @@
 	<hr>
 	<?php } ?>
 	
-	<!--delete this code -->
-	
-	<div class="container" style="overflow: hidden;">
-		<div>
-			<h2>
-				<?php
-					echo $searchResult['snippet']['title'];		
-				?>
-			</h2>
-		</div>
-		<div class="col-sm-3 col-md-6 col-lg-4 media-col">
-			<strong>
-			<?php
-				$date = new DateTime($searchResult['snippet']['publishedAt']);
-				echo $date->format('F d, Y');
-			?>
-			</strong>
-			<br/>
-			
-			<?php
-				//echo $searchResult['snippet']['description'];
-				echo 'this is livestream content description text.  i hope this works and contains enough text.';
-				
-				$videoURL = 'https://www.youtube.com/embed/'.$searchResult['id']['videoId'].'?autoplay=0&origin=http://calvarychristianchurch.com';
-			?>
-		</div>
-		
-		<div class="col-sm-3 col-md-6 col-lg-8 media-col">
-			<div class="embed-responsive embed-responsive-16by9 ytp-cued-thumbnail-overlay">
-				<iframe class="embed-responsive-item" allowFullScreen='allowFullScreen' src="<?php echo $videoURL; ?>" frameborder="0"></iframe>
-			</div>
-		</div>
-		
-	</div>
-	
-	<!--delete this code -->
 	<!--<div class="col-sm-3 col-md-6 col-lg-8 media-col">
 			<div class="embed-responsive embed-responsive-16by9 ytp-cued-thumbnail-overlay">
 				<iframe class="embed-responsive-item" src="http://www.youtube.com/embed/?listType=user_uploads&list=CCCMediaTube"></iframe>
