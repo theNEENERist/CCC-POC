9135e87023b5bc98c48f15902903b6701e00e511
Joel Leach <joel.b.leach@gmail.com>
2018-09-29 17:43:44 -0400

    read mid week study audio files from json file

--- media/studies.php
+++ media/studies.php
@@ -10,44 +10,41 @@
    <h1>Bible Studies</h1>
    <p>Checkout the recordings of our Midweek Bible studies below.  You can join us in person on Thursday evenings at 6:30 through November 15th.</p>
    <hr class="mb-4">
-
+   
    <div class="col-12 p-0">
-   <h4>Ephesians 2:1-10</h4>
-      <h6><strong>Taught By:</strong> Tom Bostic</h6>
-      <h6><strong>Date:</strong> 9/20/2018</h6>
+
+   <?php
+
+      $json = file_get_contents('../data/midweekStudies.json');
+
+      $json_data = json_decode($json,true);
+
+      usort($json_data, function ($a, $b) {
+         return strtotime($a['date']) < strtotime($b['date']);
+      });
+
+      foreach ($json_data as $key1 => $value1) {
+   ?>
+
+      <h4><?php echo $json_data[$key1]["title"]; ?></h4>
+      <h6><strong>Taught By:</strong> <?php echo $json_data[$key1]["teacher"]; ?></h6>
+      <h6><strong>Date:</strong> <?php echo $json_data[$key1]["date"]; ?></h6>
       
       <audio class="mt-3" controls controlsList="nodownload" preload="metadata">
-         <source src="../content/audio/TomBostic04.mp3" type="audio/mp3">
+         <source src="<?php echo $json_data[$key1]["file"]; ?>" type="audio/mp3">
       </audio>
 
-      <hr class="mb-4">
-
-      <h4>Ephesians 1:15-23</h4>
-      <h6><strong>Taught By:</strong> Tom Bostic</h6>
-      <h6><strong>Date:</strong> 9/20/2018</h6>
+      <?php
+         if ($key1 < count($json_data) - 1) {
+      ?>
+         <hr class="mb-4">
+      <?php
+         }
+      ?>
       
-      <audio class="mt-3" controls controlsList="nodownload" preload="metadata">
-         <source src="../content/audio/TomBostic03.mp3" type="audio/mp3">
-      </audio>
-
-      <hr class="mb-4">
-
-      <h4>Ephesians 1:1-14</h4>
-      <h6><strong>Taught By:</strong> Tom Bostic</h6>
-      <h6><strong>Date:</strong> 9/13/2018</h6>
-      <audio class="mt-3" controls controlsList="nodownload" preload="metadata">
-         <source src="../content/audio/TomBostic02.mp3" type="audio/mp3">
-      </audio>
-
-      <hr class="mb-4">
-
-      <h4>Ephesians Introduction and Overview</h4>
-      <h6><strong>Taught By:</strong> Tom Bostic</h6>
-      <h6><strong>Date:</strong> 9/6/2018</h6>      
-      <audio class="mt-3" controls controlsList="nodownload" preload="metadata">
-         <source src="../content/audio/Tom Bostic 01.mp3" type="audio/mp3">
-      </audio>
-
+   <?php
+      }
+   ?>
    </div>
 </div>
 <?php include '../inc/postContent.php'?>
