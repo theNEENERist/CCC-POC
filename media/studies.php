6956525abe0483edf3509a5537bd1908ad476be5
Joel Leach <joel.b.leach@gmail.com>
2018-11-11 21:12:58 -0500

    added call to bibles.org api for scripture references on bible studies page

--- media/studies.php
+++ media/studies.php
@@ -12,7 +12,7 @@
    <hr class="mb-4">
    
    <div class="col-12 p-0">
-
+   
    <?php
 
       $json = file_get_contents('../data/midweekStudies.json');
@@ -24,6 +24,21 @@
       });
 
       foreach ($json_data as $key1 => $value1) {
+         $token = 'HKsNrmeddiLspD4qKdqOjHGhUWVQE3HVQqCo0sCk';
+         $url = 'https://bibles.org/v2/passages.js?q[]=' . strToLower(str_replace(" ", "+", $json_data[$key1]["title"])) . '&version=eng-KJVA';
+         
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $url);
+         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+         curl_setopt($ch, CURLOPT_USERPWD, "$token:X");
+
+         $response = curl_exec($ch);
+         curl_close($ch);
+
+         $json = json_decode($response);
    ?>
       <h4><?php echo $json_data[$key1]["title"]; ?></h4>
       <h6><strong>Taught By:</strong> <?php echo $json_data[$key1]["teacher"]; ?></h6>
@@ -32,6 +47,18 @@
       <audio class="mt-3" controls controlsList="nodownload" preload="metadata">
          <source src="<?php echo $json_data[$key1]["file"]; ?>" type="audio/mp3">
       </audio>
+      
+      <?php
+         if(count($json->response->search->result->passages) > 0)  {
+      ?>
+         <button class="btn btn-secondary btn-sm collapsible mt-1" role="button">Scripture Text</button>
+         <div class="content mt-2">
+            <h1><?php echo((string) $json->response->search->result->passages[0]->display); ?></h1>
+            <?php echo((string) $json->response->search->result->passages[0]->text); ?>
+         </div>
+      <?php
+         }
+      ?>
 
       <?php
          if ($key1 < count($json_data) - 1) {
@@ -46,4 +73,23 @@
    ?>
    </div>
 </div>
+
+  <script type="text/javascript">
+   var coll = document.getElementsByClassName("collapsible");
+   var i;
+
+   for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+         this.classList.toggle("active");
+         var content = this.nextElementSibling;
+         if (content.classList.contains("transition-display")) {
+            content.classList.remove("transition-display")
+            content.classList.add("transition-hide")
+         } else {
+            content.classList.remove("transition-hide")
+            content.classList.add("transition-display")
+         }
+      });
+   }
+</script>
 <?php include '../inc/postContent.php'?>
