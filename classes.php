1eba02353ad661ddc4617c16ac3fc5f16fc7431d
Joel Leach <joel.b.leach@gmail.com>
2019-03-02 21:17:59 -0500

    WIP

--- classes.php
+++ classes.php
@@ -1,92 +1,46 @@
 <?php 
 	global $variable;
-	$imgSrc = "../img/ccc-cross-image.png";
+	$imgSrc = "./img/ccc-cross-image.png";
 ?>
 
-<?php $title = "CCC - Sermons & Worship" ?>
-<?php include 'inc/preContent.php'?>
-	
-	<div class="container">
-		<div class="col-sm-3 col-md-6 col-lg-12 overflow-auto mt-4">
-			<h1>Our Classes</h1>
-			<p>We have classes available on Sunday and Thursday mornings; as well as Wednesday evenings.</p>
-		</div>
-		<div>
-		
-		<?php
-		   // $dbhost = '127.0.0.1:3036';
-		   // $dbuser = 'root';
-		   // $dbpass = 'narnia3#';
-		   // $database = 'ccc';
-		   
-			// $host="localhost";
-			// $port=3306;
-			// $socket="";
-			// $user="root";
-			// $password="";
-			// $dbname="";
+<?php $title = "CCC - Classes" ?>
+<?php include './inc/preContent.php'?>
 
-			$mysqli = new mysqli($host, $user, $password, $dbname, $port, $socket)
-				or die ('Could not connect to the database server' . mysqli_connect_error());
+<div class="container">
+  <h1>Classes</h1>
+  <p>Check out the classes we offer.</p>
+  <hr class="mb-4">
+  
+  <div class="col-12 p-0">
+    <?php
+      $json = file_get_contents('./data/classes.json');
 
-			$sql = 'SELECT 
-				teacher
-				,topic
-				,classType
-			FROM ccc.class';
+      $json_data = json_decode($json,true);
 
-			$result = $mysqli->query($sql);
-			foreach ($result as $row)
-			{
-			?>
-			<div class="col-3 float-left text-center bg-blue text-white rounded-border pd-5">
-			<?php
-				echo $row['topic'] . "<br/>";
-				echo $row['teacher'] . "<br/>";
-			?>
-				</div>
-			
-			<?php
-			}
+      usort($json_data, function ($a, $b) {
+         return strtotime($a['date']) < strtotime($b['date']);
+      });
 
+      for ($x = 0; $x < 1; $x++) {
+   ?>
+      <h4><?php echo $json_data[$x]["subject"]; ?></h4>
+      <h6><strong>Taught By:</strong> <?php echo $json_data[$x]["teacher"]; ?></h6>
+      <h6><strong>Date:</strong> <?php echo $json_data[$x]["when"]; ?></h6>
+      
+      <?php
+      
+        if ($x < count($json_data) - 1) {
 
-// $mysqli->query($sql);
-		   // $retval = mysql_query( $sql, $conn );
-		   
-		   // if(! $retval ) {
-			  // die('Could not get data: ' . mysql_error());
-		   // }
-		   
-		   
-		   
-		   
-		   mysqli_close($mysqli);
-		?>
-		
-		
-			
-			<!--<div class="col-sm-3 col-md-6 col-lg-4 float-left text-center">
-				<h4>Sunday - 9:30 AM</h4>
-				<hr/>
-				<div>
-					Learn the Bible in 24 Hours
-					<br/>
-					Jack Warner
-				</div>
-			</div>
-			
-			<div class="col-sm-3 col-md-6 col-lg-4 float-left text-center">
-				<h4>Wednesday - 6:30 PM</h4>
-				<hr/>
-			</div>
-			
-			
-			<div class="col-sm-3 col-md-6 col-lg-4 float-left text-center">
-				<h4>Thursday - 10:00 PM</h4>
-				<hr/>
-			</div>-->
-			
-		</div>
-	<div>
+      ?>
+        <hr class="mb-4">
+      <?php          
+        }
+      ?>
+      <span></span>
+   <?php
+      }
+   ?>
+   </div>
+</div>
 
-<?php include 'inc/postContent.php'?>
+<?php include './inc/postContent.php'?>
