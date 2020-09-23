75d41e478876c996bea7e09a467fada6c40418a0
Joel Leach <joel.b.leach@gmail.com>
2020-09-22 22:40:16 -0400

    wip

--- inc/postContent.php
+++ inc/postContent.php
@@ -78,6 +78,25 @@
 		<script src="/../js/bootstrap.min.js"></script>
 		
 		<script type="text/javascript">
+			window.addEventListener('scroll', () => {
+				let page = this;
+				let pageTop = this.scrollY;
+				let pageHeight = this.outerHeight / 2 ;
+				
+				let frames = document.querySelectorAll('.scrollAnim');
+				frames.forEach( frame => {
+					let frameTop = frame.offsetTop;
+					let frameHeight = frame.offsetHeight;
+					
+					if ( pageTop  >= frameTop - pageHeight &&
+						pageTop  < frameTop + frameHeight/2 ){
+						frame.classList.add('anim');
+					}else{
+						frame.classList.remove('anim');
+					}
+				});
+			});
+			
 			jQuery(window).scroll(function(){
 				var fromTopPx;
 				var screenWidth = $(window).width();
