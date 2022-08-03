3291b356cb7eda35757314e763295c8783ce5528
Joel Leach <joel.b.leach@gmail.com>
2022-08-02 23:18:09 -0400

    added rich, removed kevin

--- our_leaders.php
+++ our_leaders.php
@@ -28,55 +28,47 @@
 		At the end of the day, the CCC leadership hopes that the office work our staff completes reflects God and brings the members and attendees that much closer to the Lord.
 	</div>
 	
-	<hr/>
-	
-	<div class="overflow-auto">
-		<div class="col overflow-auto mb-2">
-			<h3>Preaching Minister</h3>
-		</div>
-		
-		<div class="col-sm-5 col-lg-2 overflow-auto mb-4 float-left">
-			<img src="../img/our-leadership-kevin.webp" alt="Kevin - Minister" >
-		</div>
-		
-		<div class="col-sm-7 col-lg-10 overflow-auto mb-4 float-left">
-			<strong>Kevin R. Beck</strong>
-			<p>Minister
-			<br/>
-			kevin@calvarychristianchurch.com</p>
-			
-			<div class="overflow-auto">
-				Kevin began his ministry with Calvary Christian Church as a part time interim Youth Minister. We liked him so much, we hired him permanently. He began his ministry with us in the fall of 2007. On March 21, 2016, CCC transitioned Kevin into our associate minister and on January 1, 2018 he became our full-time preaching minister. Kevin is a "home grown product." He graduated from our own Silver Creek High School and grew up in Memphis Christian Church, a sister church just seven miles from us. He is a 1993 graduate of Cincinnati Bible College (now Cincinnati Christian University). In twenty years of ministry, he has served congregations in the areas of youth, music, and preaching. He has also taught for several years in Christian schools, and spent almost five years as a missionary in Mozambique. He and his wife Mindy live just a few miles away in New Albany.
-			</div>
-		</div>
-	</div>
-	
-	<hr/>
-	
+
 	<div class="mt-4 overflow-auto">
 		<div class="col overflow-auto">
-			<h3>Elders/Pastors</h3>
+			<h2>Elders</h2>
 		</div>
 		
-		<div class="col-lg-4 overflow-auto float-left text-center">
+		<div class="ml-3 float-left">
 			<img src="../img/our-leadership-james.webp" alt="Marty - Elder">
 			<br/>
 			James Hill
 		</div>
 		
-		<div class="col-lg-4 overflow-auto float-left text-center">
+		<div class="ml-3 float-left">
 			<img src="../img/our-leadership-gary-t.webp" alt="Gary - Elder">
 			<br/>
 			Gary Thieneman
 		</div>
+	</div>
+
+	
+	<div class="mt-4 overflow-auto">
+		<div class="col overflow-auto mb-2">
+			<h2>Ministers</h2>
+		</div>
 		
-		<div class="col-lg-4 overflow-auto float-left text-center">
-			<img src="../img/our-leadership-jack.webp" alt="Jack - Elder">
+		<div class="col-sm-5 col-lg-2 overflow-auto mb-4 float-left">
+			<img src="../img/our-leadership-rich.webp" alt="Kevin - Minister" >
+		</div>
+		
+		<div class="col-sm-7 col-lg-10 overflow-auto mb-4 float-left">
+			<strong>Rich Hoyer</strong>
+			<p>Senior Minister
 			<br/>
-			Jack Warner
+			Rich@calvarychristianchurch.com</p>
+			
+			<div class="overflow-auto">
+					Rich and his family began their ministry with Calvary Chrisitan Church in July of 2022. After graduating from the University of Louisville with a degree in Economics in 1998, he became a Christian and returned to school to earn a Master’s in Religion from Cincinnati Christian University in 2006 <br/>
+					He is the founder of the <a target="_blank" href="https://reveal-truth.com">Reveal Conference <i class="fas fa-external-link-alt" style="color: #3376BC"></i></a> in Louisville, KY—an organization that seeks to educate people about the evidence for the truth of Christianity. Before the Lord led Rich to us, he served as a minister of the gospel at Lyndon Christian Church in Louisville, KY for over 14 years. You can find his writing at <a target="_blank" href="https://www.illuminatingchristianity.com">IlluminatingChristianity.com <i class="fas fa-external-link-alt" style="color: #3376BC"></i></a>.
+			</div>
 		</div>
 	</div>
-	
 </div>
 
 <?php include 'inc/postContent.php'?>
